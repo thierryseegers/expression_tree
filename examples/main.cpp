96d5c5ff5f1614c1f514869d47f1cc2f956d77b1
Thierry Seegers <tseegers@fluential.com>
2014-05-01 15:56:10 -0700

    Replaced busy_1_sec with a simpler lambda inside the main, near the point of use. Added example for a tree with a leaf that is assigned a lambda. Reverted all whitespace to tabs.

--- examples/main.cpp
+++ examples/main.cpp
@@ -6,310 +6,262 @@
 #include <string>
 #include <thread>
 
-#if defined(_WIN32)
-#include <Windows.h>
-#elif defined(__linux__)
-#include <sys/resource.h>
-#include <sys/time.h>
-#include <unistd.h>
-#elif defined(__APPLE__)
-#include <mach/mach_init.h>
-#include <mach/task.h>
-#include <mach/task_info.h>
-#endif
-
 using namespace std;
-
-// This function keeps the thread it is run on busy for one second's worth of CPU time.
-// Implementations are provided for major platforms.
-nullptr_t busy_1_sec(const nullptr_t&, const nullptr_t&)
-{
-#if defined(_WIN32)
-
-    FILETIME a, b, kernel_ft, user_ft;
-    GetThreadTimes(GetCurrentThread(), &a, &b, &kernel_ft, &user_ft);
-
-    ULARGE_INTEGER kernel_start, kernel_elapsed, user_start, user_elapsed;
-
-    kernel_start.HighPart = kernel_ft.dwHighDateTime;
-    kernel_start.LowPart = kernel_ft.dwLowDateTime;
-
-    user_start.HighPart = user_ft.dwHighDateTime;
-    user_start.LowPart = user_ft.dwLowDateTime;
-
-    do
-    {
-        short i = 0; while(++i);
-
-        GetThreadTimes(GetCurrentThread(), &a, &b, &kernel_ft, &user_ft);
-
-        kernel_elapsed.HighPart = kernel_ft.dwHighDateTime;
-        kernel_elapsed.LowPart = kernel_ft.dwLowDateTime;
-
-        user_elapsed.HighPart = user_ft.dwHighDateTime;
-        user_elapsed.LowPart = user_ft.dwLowDateTime;
-    }
-    while(((kernel_elapsed.QuadPart + user_elapsed.QuadPart) - (kernel_start.QuadPart + user_start.QuadPart)) < 10000000);
-
-#elif defined(__linux__)
-    rusage r;
-    getrusage(RUSAGE_THREAD, &r);
-
-    double start, elapsed;
-    start = r.ru_utime.tv_sec + r.ru_stime.tv_sec + ((r.ru_utime.tv_usec + r.ru_stime.tv_usec) / 1000000.);
-    
-    do
-    {
-        char c = 0; while(++c);
-
-        getrusage(RUSAGE_THREAD, &r);
-
-        elapsed = r.ru_utime.tv_sec + r.ru_stime.tv_sec + ((r.ru_utime.tv_usec + r.ru_stime.tv_usec) / 1000000.);
-    }
-    while(elapsed - start < 1.);
-    
-#elif defined(__APPLE__)
-    task_thread_times_info info;
-    mach_msg_type_number_t number = TASK_THREAD_TIMES_INFO_COUNT;
-    task_info(mach_task_self(), TASK_THREAD_TIMES_INFO, (task_info_t)&info, &number);
-    
-    double start, elapsed;
-    start = info.user_time.seconds + info.system_time.seconds + ((info.user_time.microseconds + info.system_time.microseconds) * 1e-6);
-    
-    do
-    {
-        char c = 0; while(++c);
-        
-        task_info(mach_task_self(), TASK_THREAD_TIMES_INFO, (task_info_t)&info, &number);
-        
-        elapsed = info.user_time.seconds + info.system_time.seconds + ((info.user_time.microseconds + info.system_time.microseconds) * 1e-6);
-    }while(elapsed - start < 1.);
-    
-#endif
-
-    return nullptr;
-};
 
 int main()
 {
-    // Create an int tree that will not cache values.
-    expression_tree::tree<int, expression_tree::no_caching> tinc;
+	// Create an int tree that will not cache values.
+	expression_tree::tree<int, expression_tree::no_caching> tinc;
 
-    // Let's build the simplest of trees, a singe leaf:
-    //
-    //        3
+	// Let's build the simplest of trees, a singe leaf:
+	//
+	//		3
 
-    tinc.root() = 3;
+	tinc.root() = 3;
 
-    cout << tinc.evaluate() << endl; // Prints "3".
-
-
-    // Let's build a more complex tree:
-    //
-    //  (2 * l + r)
-    //   /       \
-    //  1      (l - r)
-    //          /   \
-    //        2      3
-
-    tinc.root() = [](int i, int j){ return 2 * i + j; };
-
-    tinc.root().left() = 1;
-    tinc.root().right() = minus<int>();
-    tinc.root().right().left() = 2;
-    tinc.root().right().right() = 3;
-
-    cout << tinc.evaluate() << endl; // Prints "1" (2 * 1 + (2 - 3)).
-
-    // Re-evaluate the tree.
-    // Because it is a non-caching tree, all nodes will be re-visited and all operations re-applied.
-    cout << tinc.evaluate() << endl;
-
-    // Let's change the tree a bit. Make one leaf a variable:
-    //
-    //  (2 * l + r)
-    //   /       \
-    //  1      (l - r)
-    //          /   \
-    //        2      x
-
-    // Assign a variable to the rightmost leaf.
-    int x = 1;
-    tinc.root().right().right() = &x;
-
-    cout << tinc.evaluate() << endl; // Prints "3" (2 * 1 + (2 - 1)).
-
-    // Change the value of that variable and re-evaluate.
-    x = 2;
-
-    cout << tinc.evaluate() << endl; // Prints "2" (2 * 1 + (2 - 2)).
+	cout << tinc.evaluate() << endl; // Prints "3".
 
 
-    // Create a string tree with caching-on-evaluation optimization.
-    expression_tree::tree<string, expression_tree::cache_on_evaluation> tsce;
-    
-    // Let's build this tree:
-    //
-    //    (l + r)
-    //  /         \
-    // s        (l + r)
-    //           /   \
-    //         " "  "tree"
+	// Let's build a more complex tree:
+	//
+	//  (2 * l + r)
+	//   /		 \
+	//  1	  (l - r)
+	//		   /   \
+	//		  2		3
 
-    string s = "expression";
+	tinc.root() = [](int i, int j){ return 2 * i + j; };
 
-    tsce.root() = plus<string>();
-    tsce.root().left() = &s;
-    tsce.root().right() = plus<string>();
-    tsce.root().right().left() = string(" ");
-    tsce.root().right().right() = string("tree");
+	tinc.root().left() = 1;
+	tinc.root().right() = minus<int>();
+	tinc.root().right().left() = 2;
+	tinc.root().right().right() = 3;
 
-    cout << tsce.evaluate() << endl; // Prints "expression tree".
+	cout << tinc.evaluate() << endl; // Prints "1" (2 * 1 + (2 - 3)).
 
-    // Change the value of variable s and re-evaluate.
-    // Because this is a caching tree, constant branches will not be re-evaluated.
-    // For this tree, it means the concatenation of " " and "tree" will not be performed again.
-    s = "apple";
+	// Re-evaluate the tree.
+	// Because it is a non-caching tree, all nodes will be re-visited and all operations re-applied.
+	cout << tinc.evaluate() << endl;
 
-    cout << tsce.evaluate() << endl; // Prints "apple tree".
+	// Let's change the tree a bit. Make one leaf a variable:
+	//
+	//  (2 * l + r)
+	//   /		 \
+	//  1	  (l - r)
+	//		   /   \
+	//		  2		x
 
-    // But hwat happens if we do change the value of one the leaves that had a constant value?
-    // The tree will do the right thing, and discard the previously cached value.
+	// Assign a variable to the rightmost leaf.
+	int x = 1;
+	tinc.root().right().right() = &x;
 
-    tsce.root().right().right() = string("pie");
+	cout << tinc.evaluate() << endl; // Prints "3" (2 * 1 + (2 - 1)).
 
-    cout << tsce.evaluate() << endl; // Prints "apple pie".
+	// Change the value of that variable and re-evaluate.
+	x = 2;
+
+	cout << tinc.evaluate() << endl; // Prints "2" (2 * 1 + (2 - 2)).
+
+	// Let's do the same thing but with a callable instead.
+	//
+	//  (2 * l + r)
+	//   /		 \
+	//  1	  (l - r)
+	//		   /   \
+	//		  2		f()
+	
+	// Assign a callable to the rightmost leaf.
+	x = 1;
+	tinc.root().right().right() = [&x]{ return x; };
+	
+	cout << tinc.evaluate() << endl; // Prints "3" (2 * 1 + (2 - 1)).
+	
+	// Change the value of that variable and re-evaluate.
+	x = 2;
+	
+	cout << tinc.evaluate() << endl; // Prints "2" (2 * 1 + (2 - 2)).
+	
+
+	// Create a string tree with caching-on-evaluation optimization.
+	expression_tree::tree<string, expression_tree::cache_on_evaluation> tsce;
+	
+	// Let's build this tree:
+	//
+	//	(l + r)
+	//	 /	 \
+	//	s	(l + r)
+	//		 /   \
+	//		" "  "tree"
+
+	string s = "expression";
+
+	tsce.root() = plus<string>();
+	tsce.root().left() = &s;
+	tsce.root().right() = plus<string>();
+	tsce.root().right().left() = string(" ");
+	tsce.root().right().right() = string("tree");
+
+	cout << tsce.evaluate() << endl; // Prints "expression tree".
+
+	// Change the value of variable s and re-evaluate.
+	// Because this is a caching tree, constant branches will not be re-evaluated.
+	// For this tree, it means the concatenation of " " and "tree" will not be performed again.
+	s = "apple";
+
+	cout << tsce.evaluate() << endl; // Prints "apple tree".
+
+	// But hwat happens if we do change the value of one the leaves that had a constant value?
+	// The tree will do the right thing, and discard the previously cached value.
+
+	tsce.root().right().right() = string("pie");
+
+	cout << tsce.evaluate() << endl; // Prints "apple pie".
 
 
-    // Demonstration of a caching-on-modification tree with its degenerate case.
+	// Demonstration of a caching-on-modification tree with its degenerate case.
 
-    // Let's build this tree:
-    //
-    //
-    // (l + r)
-    //  /   \
-    // 1   (l + r)
-    //      /   \
-    //     2   (l + r)
-    //          /   \
-    //         3     4
+	// Let's build this tree:
+	//
+	//
+	// (l + r)
+	//  /   \
+	// 1   (l + r)
+	//		/   \
+	//	   2   (l + r)
+	//			/   \
+	//		   3	 4
 
-    expression_tree::tree<int, expression_tree::cache_on_assignment> tica;
+	expression_tree::tree<int, expression_tree::cache_on_assignment> tica;
 
-    tica.root() = plus<int>();
-    tica.root().left() = 1;
-    tica.root().right() = plus<int>();
-    tica.root().right().left() = 2;
-    tica.root().right().right() = plus<int>();
-    tica.root().right().right().left() = 3;
-    tica.root().right().right().right() = 4; // Right here, the entire tree will be pre-evaluated.
+	tica.root() = plus<int>();
+	tica.root().left() = 1;
+	tica.root().right() = plus<int>();
+	tica.root().right().left() = 2;
+	tica.root().right().right() = plus<int>();
+	tica.root().right().right().left() = 3;
+	tica.root().right().right().right() = 4; // Right here, the entire tree will be pre-evaluated.
 
-    cout << tica.evaluate() << endl; // Prints "10". Even the first time this tree is evaluated, its value is already cached!
+	cout << tica.evaluate() << endl; // Prints "10". Even the first time this tree is evaluated, its value is already cached!
 
-    tica.root().right().right().right() = 5; // Again, the entire tree will be pre-evaluated.
+	tica.root().right().right().right() = 5; // Again, the entire tree will be pre-evaluated.
 
-    cout << tica.evaluate() << endl; // Prints "11".
+	cout << tica.evaluate() << endl; // Prints "11".
 
 
-    // Here's an example of copying a tree (or sub-tree) to another tree's node.
-    expression_tree::tree<int, expression_tree::cache_on_evaluation> tice;
+	// Here's an example of copying a tree (or sub-tree) to another tree's node.
+	expression_tree::tree<int, expression_tree::cache_on_evaluation> tice;
 
-    // We first build a simple tree:
-    //
-    //  (l + r)
-    //  /    \
-    // y      2
+	// We first build a simple tree:
+	//
+	//  (l + r)
+	//  /	\
+	// y	  2
 
-    int y = 2;
+	int y = 2;
 
-    tice.root() = plus<int>();
-    tice.left() = &y;
-    tice.right() = 2;
+	tice.root() = plus<int>();
+	tice.left() = &y;
+	tice.right() = 2;
 
-    cout << tice.evaluate() << endl; // Prints "4" (y + 2).
+	cout << tice.evaluate() << endl; // Prints "4" (y + 2).
 
-    // Then we build on it (using its own nodes!):
-    //
-    //     (l + r)
-    //      /   \
-    // (l + r)   2
-    //  /   \
-    // y     2
+	// Then we build on it (using its own nodes!):
+	//
+	//	 (l + r)
+	//	  /	   \
+	// (l + r)  2
+	//  /   \
+	// y	 2
 
-    expression_tree::node<int, expression_tree::cache_on_evaluation> n = tice.root();
+	expression_tree::node<int, expression_tree::cache_on_evaluation> n = tice.root();
 
-    tice.left() = n;
+	tice.left() = n;
 
-    // Let's make it even bigger:
-    //
-    //     (l + r)
-    //      /   \
-    // (l + r)  (l + r)
-    //  /   \    /   \
-    // y     2  y     2
+	// Let's make it even bigger:
+	//
+	//	 (l + r)
+	//	  /   \
+	// (l + r)  (l + r)
+	//  /   \	 /   \
+	// y	 2  y	 2
 
-    tice.right() = n;
+	tice.right() = n;
 
-    cout << tice.evaluate() << endl; // Prints "8" ((y + 2) + (y + 2)).
+	cout << tice.evaluate() << endl; // Prints "8" ((y + 2) + (y + 2)).
 
-    // Demonstration of parallel evaluation.
-    //
-    // We build two trees with the exact same morphology, one to be evaluated sequentially and another, parallely.
-    // Nodes operation is to stay busy for one second.
-    // Leaves have no value of consequence.
-    //
-    //              busy 1s              // Level 1
-    //             /       \
-    //      busy 1s         busy 1s      // Level 2
-    //      /     \         /     \  
-    // busy 1s busy 1s busy 1s busy 1s   // Level 3
-    //  /   \   /   \   /   \   /   \ 
-    // 0     0 0     0 0     0 0     0   // Level 4	(instantaneous evaluation)
-    //
-    // It should take 7 seconds to evaluate the sequential tree, but less time to evaluate the parallel one.
-    // If one's hardware can execute two threads in parallel, level 3 can be evaluated in two seconds and level 2 in one second.
-    // If one's hardware can execute four threads in parallel, level 3 and level 2 can each be evaluated in one second.
+	// Demonstration of parallel evaluation.
+	//
+	// We build two trees with the exact same morphology, one to be evaluated sequentially and another, parallely.
+	// Branches operations are to stay busy for one second. This simulates expensive computations on values.
+	// Leaves have no value of consequence for this example.
+	//
+	//			  busy 1s				// Level 1
+	//			 /	   \
+	//	  busy 1s		 busy 1s		// Level 2
+	//	  /		\		 /	   \
+	// busy 1s busy 1s busy 1s busy 1s	// Level 3
+	//  /   \   /   \   /   \   /   \ 
+	// 0	 0 0	 0 0	 0 0	 0	// Level 4 (instantaneous evaluation)
+	//
+	// It should take 7 seconds to evaluate the sequential tree, but less time to evaluate the parallel one.
+	// If one's hardware can execute two threads in parallel, level 3 can be evaluated in two seconds and level 2 in one second.
+	// If one's hardware can execute four threads in parallel, level 3 and level 2 can each be evaluated in one second.
 
-    // The sequential tree.
-    expression_tree::tree<nullptr_t, expression_tree::no_caching, expression_tree::sequential> tnncl;
-    tnncl.root() = &busy_1_sec;
-    tnncl.left() = &busy_1_sec;
-    tnncl.left().left() = &busy_1_sec;
-    tnncl.left().left().left() = nullptr;
+	// Here's our branch operation. It stays busy doing "work" for one second.
+	auto busy_1_sec = [](const nullptr_t&, const nullptr_t&)
+	{
+		auto then = chrono::high_resolution_clock::now();
+		
+		while((chrono::high_resolution_clock::now() - then) < 1s)
+		{
+			short i = 0; while(++i);
+		}
+		
+		return nullptr;
+	};
 
-    tnncl.left().left().right() = tnncl.left().left().left();
-    tnncl.left().right() = tnncl.left().left();
-    tnncl.right() = tnncl.left();
+	// The sequential tree.
+	expression_tree::tree<nullptr_t, expression_tree::no_caching, expression_tree::sequential> tnncl;
+	
+	tnncl.root() = busy_1_sec;
+	tnncl.left() = busy_1_sec;
+	tnncl.left().left() = busy_1_sec;
+	tnncl.left().left().left() = nullptr;
+	
+	tnncl.left().left().right() = tnncl.left().left().left();
+	tnncl.left().right() = tnncl.left().left();
+	tnncl.right() = tnncl.left();
 
-    auto then = chrono::high_resolution_clock::now();
-    tnncl.evaluate();
-    cout << "Sequential tree evaluated in " << chrono::duration<float>(chrono::high_resolution_clock::now() - then).count() << " seconds.\n";    // 7 seconds.
+	auto then = chrono::high_resolution_clock::now();
+	tnncl.evaluate();
+	cout << "Sequential tree evaluated in " << chrono::duration<float>(chrono::high_resolution_clock::now() - then).count() << " seconds.\n";	// 7 seconds.
 
-    // The parallel tree.
-    expression_tree::tree<nullptr_t, expression_tree::no_caching, expression_tree::parallel> tnncp;
-    tnncp.root() = &busy_1_sec;
-    tnncp.left() = &busy_1_sec;
-    tnncp.left().left() = &busy_1_sec;
-    tnncp.left().left().left() = nullptr;
+	// The parallel tree.
+	expression_tree::tree<nullptr_t, expression_tree::no_caching, expression_tree::parallel> tnncp;
+	tnncp.root() = busy_1_sec;
+	tnncp.left() = busy_1_sec;
+	tnncp.left().left() = busy_1_sec;
+	tnncp.left().left().left() = nullptr;
 
-    tnncp.left().left().right() = tnncp.left().left().left();
-    tnncp.left().right() = tnncp.left().left();
-    tnncp.right() = tnncp.left();
+	tnncp.left().left().right() = tnncp.left().left().left();
+	tnncp.left().right() = tnncp.left().left();
+	tnncp.right() = tnncp.left();
 
-    then = chrono::high_resolution_clock::now();
-    tnncp.evaluate();
-    cout << "Parallel tree evaluated in " << chrono::duration<float>(chrono::high_resolution_clock::now() - then).count() << " seconds.\n";  // 3 seconds on my computer.
+	then = chrono::high_resolution_clock::now();
+	tnncp.evaluate();
+	cout << "Parallel tree evaluated in " << chrono::duration<float>(chrono::high_resolution_clock::now() - then).count() << " seconds.\n";  // 3 seconds on my computer.
 
-    // Misues.
 
-    expression_tree::tree<float> crash;
-    //crash.evaluate(); // The tree is empty.
+	// Misues.
 
-    crash.root() = divides<float>();
-    //crash.evaluate(); // This tree cannot be evaluated, its root node is an operation with no data children to operate on.
+	expression_tree::tree<float> crash;
+	//crash.evaluate(); // The tree is empty.
 
-    crash.root() = 2.;
-    //crash.root().left() = divides<float>(); // Assigning to a leaf node's children is undefined behavior.
+	crash.root() = divides<float>();
+	//crash.evaluate(); // This tree cannot be evaluated, its root node is an operation with no data children to operate on.
 
-    return 0;
+	crash.root() = 2.;
+	//crash.root().left() = divides<float>(); // Assigning to a leaf node's children is undefined behavior.
+
+	return 0;
 }
